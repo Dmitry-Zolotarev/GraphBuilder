8904e026be0f6ef35af2c5002f1c4a641c1186c9
Dmitry-Zolotarev <dimazed04@gmail.com>
2025-04-16 20:18:27 +0700

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace SKV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(984, 641);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseWheel);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(15, 689);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(981, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 666);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Введите функцию";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(176, 666);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Масштаб: 1x";
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 1000;
+            this.toolTip1.InitialDelay = 500;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Построитель графиков функций";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Mathos.Parser;
+using System.Text.RegularExpressions;
+
+namespace SKV
+{
+    public partial class Form1 : Form
+    {
+        private MathParser parser;
+        double scale = 10; // Масштаб графика
+        int centerX = 0, centerY = 0, offsetX = 0, offsetY = 0; // Смещение графика
+        Point lastMousePos; // Последняя позиция мыши при перетаскивании
+
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.BackColor = Color.White;
+
+            // Инициализация парсера
+            parser = new MathParser();
+
+            // Добавляем кастомную функцию pow(x, y)
+            parser.LocalFunctions["pow"] = args => Math.Pow(args[0], args[1]);
+            buildGraph();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            offsetX = 0;
+            offsetY = 0;
+            buildGraph();
+        }
+        private double f(double x)
+        {
+            double y = 0;
+            try
+            {
+                parser.LocalVariables["x"] = x;
+                var function = Regex.Replace(textBox1.Text, @"([a-zA-Z0-9_\.]+)\s*\^\s*([0-9\.]+)", "pow($1,$2)");
+                y = parser.Parse(function);
+            }
+            catch(Exception) {}
+            return y;
+        }
+
+        
+
+        private void buildGraph()
+        {
+            try
+            {
+                var function = Regex.Replace(textBox1.Text, @"([a-zA-Z0-9_\.]+)\s*\^\s*([0-9\.]+)", "pow($1,$2)");
+                int w = pictureBox1.Width, h = pictureBox1.Height;
+                Bitmap bmp = new Bitmap(w, h);
+                Graphics g = Graphics.FromImage(bmp);
+                g.Clear(Color.White);
+
+                centerX = w / 2 + offsetX;
+                centerY = h / 2 + offsetY;
+
+                // Рисуем оси
+                g.DrawLine(new Pen(Color.Gray, 1), 0, centerY, w, centerY); // X-axis
+                g.DrawLine(new Pen(Color.Gray, 1), centerX, 0, centerX, h); // Y-axis
+
+
+                // Отрисовка делений по оси X
+                double step = 60 / scale; // адаптивный шаг, можно сделать динамическим
+                double xStart = -((centerX) / scale);
+                double xEnd = (w - centerX) / scale;
+
+                for (double x = Math.Floor(xStart / step) * step; x <= xEnd; x += step)
+                {
+                    int screenX = (int)(centerX + x * scale);
+                    g.DrawLine(Pens.LightGray, screenX, centerY - 5, screenX, centerY + 5);
+
+                    if (Math.Abs(x) > 1e-6)
+                        g.DrawString(Math.Round(x / 3, 2).ToString(), this.Font, Brushes.Black, screenX + 2, centerY + 5);
+                }
+
+                // Отрисовка делений по оси Y
+                double yStart = -((h - centerY) / scale);
+                double yEnd = (centerY) / scale;
+
+                for (double y = Math.Floor(yStart / step) * step; y <= yEnd; y += step)
+                {
+                    int screenY = (int)(centerY - y * scale);
+                    g.DrawLine(Pens.LightGray, centerX - 5, screenY, centerX + 5, screenY);
+
+                    if (Math.Abs(y) > 1e-6)
+                        g.DrawString(Math.Round(y / 3, 2).ToString(), this.Font, Brushes.Black, centerX + 5, screenY - 10);
+                }
+
+                // Рисуем график функции
+                for (int px = 0; px < w - 1; px++)
+                {
+                    double x1 = (px - centerX) / scale;
+                    double x2 = (px + 1 - centerX) / scale;
+
+                    parser.LocalVariables["x"] = x1;
+                    double y1;
+                    try { y1 = parser.Parse(function); }
+                    catch { continue; }
+
+                    parser.LocalVariables["x"] = x2;
+                    double y2;
+                    try { y2 = parser.Parse(function); }
+                    catch { continue; }
+
+                    int py1 = centerY - (int)(y1 * scale);
+                    int py2 = centerY - (int)(y2 * scale);
+
+                    if (py1 >= 0 && py1 < h && py2 >= 0 && py2 < h)
+                    {
+                        g.DrawLine(new Pen(Color.Blue, 2), px, py1, px + 1, py2);
+                    }
+                }
+                // Отображаем полученное изображение на PictureBox
+                pictureBox1.Image = bmp;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e) => buildGraph();
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            // Перетаскивание графика
+            if (e.Button == MouseButtons.Left)
+            {
+                toolTip1.Hide(pictureBox1);
+                int dx = e.X - lastMousePos.X;
+                int dy = e.Y - lastMousePos.Y;
+               
+                offsetX += dx;
+                offsetY += dy;
+                buildGraph();
+            }
+            else
+            {
+                double x = (e.X - centerX + offsetX) / scale;
+                toolTip1.Show($"f({Math.Round(x, 4)}) = {Math.Round(f(x), 4)}", pictureBox1, lastMousePos.X + 15, lastMousePos.Y + 15, 1000);
+            }
+            lastMousePos = e.Location;
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            double x = (lastMousePos.X - centerX + offsetX) / scale;
+            try
+            {
+                
+            }
+            catch (Exception ) {  }
+           
+        }
+        private void pictureBox1_MouseWheel(object sender, MouseEventArgs e)
+        {
+            // Масштабирование графика
+            if (scale > 1 && e.Delta < 0 || scale < 1000 && e.Delta >= 0) scale += e.Delta / 100;
+            label2.Text = "Масштаб: " + Math.Round(scale / 20, 2) + "x";
+            buildGraph();
+        }
+
+    }
+}
